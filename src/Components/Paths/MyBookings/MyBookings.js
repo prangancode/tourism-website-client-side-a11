25edcbf2493778b77c7e110e7aa484c46ec86863
prangancode <senprangan@gmail.com>
2021-10-30 22:05:16 +0600

    first commit

---
+++ src/Components/Paths/MyBookings/MyBookings.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import MyBookingStyle from './MyBookingStyle';
+
+const MyBookings = () => {
+    const [myBookings, setMyBookings] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/submitbookings')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setMyBookings(data);
+            });
+    }, [])
+    return (
+        <div>
+            {/* <h1>My Bookings</h1> */}
+            {/* <h1>length : {myBookings.length}</h1> */}
+            <div className="gap-8 grid lg:grid-cols-3 p-36 sm:p-20">
+                {
+                    myBookings.map(myBooking => <MyBookingStyle setMyBookings={setMyBookings} myBookings={myBookings} myBooking={myBooking} key={myBooking._id}></MyBookingStyle>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default MyBookings;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyApYAMGQ8ARafSsQF5qLsVXNOh_HH2tTNk",
+    authDomain: "tourism-website-cf4cd.firebaseapp.com",
+    projectId: "tourism-website-cf4cd",
+    storageBucket: "tourism-website-cf4cd.appspot.com",
+    messagingSenderId: "923685650561",
+    appId: "1:923685650561:web:619b73435b26cd421d0d80"
+};
+
+export default firebaseConfig;
