a949a64d7c895421c0eb39be800cbc00890b40eb
prangancode <senprangan@gmail.com>
2021-10-31 18:57:55 +0600

    header and footer file design added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,11 +4,7 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div style={{
-            backgroundImage: `url("https://gotravel.qodeinteractive.com/wp-content/uploads/2016/11/client-22.png")`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center'
-        }} className="footer px-4 divide-y bg-green-100 dark:text-coolGray-100">
+        <div className="footer px-4 divide-y bg-green-100 dark:text-coolGray-100">
             <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
                 <div className="lg:w-1/3">
                     <a href="https:www.google.com" className="flex justify-center space-x-3 lg:justify-start">
@@ -17,12 +13,12 @@ const Footer = () => {
                                 <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
                             </svg>
                         </div>
-                        <span className="self-center text-2xl font-semibold">Travelata</span>
+                        <span className="self-center text-3xl font-bold text-green-900">Travelata</span>
                     </a>
                 </div>
                 <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
                     <div className="space-y-3">
-                        <h3 className="tracking-wide uppercase dark:text-coolGray-50">Learn More</h3>
+                        <h3 className="uppercase text-green-900 font-serif font-bold lg:text-lg mb-4">Learn More</h3>
                         <ul className="space-y-1">
                             <li>
                                 <Link to='/courses'>About Travelata</Link>
@@ -45,7 +41,7 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="space-y-3">
-                        <h3 className="tracking-wide uppercase dark:text-coolGray-50">Our Services</h3>
+                        <h3 className="uppercase text-green-900 font-serif font-bold lg:text-lg mb-4">Our Services</h3>
                         <ul className="space-y-1">
                             <li>
                                 <a href="https:www.google.com">Tickets & Booking</a>
@@ -69,21 +65,21 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="space-y-3">
-                        <h3 className="uppercase dark:text-coolGray-50">Developers</h3>
+                        <h3 className="uppercase text-green-900 font-serif font-bold lg:text-lg mb-4">Useful Link</h3>
                         <ul className="space-y-1">
                             <li>
                                 <a href="https:www.google.com">Public API</a>
                             </li>
                             <li>
-                                <a href="https:www.google.com">Documentation</a>
+                                <a href="https:www.google.com">Guides</a>
                             </li>
                             <li>
-                                <a href="https:www.google.com">Guides</a>
+                                <a href="https:www.google.com">Documentation</a>
                             </li>
                         </ul>
                     </div>
                     <div className="space-y-3">
-                        <div className="uppercase dark:text-coolGray-50">Social media</div>
+                        <div className="uppercase text-green-900 font-serif font-bold lg:text-lg mb-4">Social media</div>
                         <div className="flex justify-start lg:space-x-2 sm:space-x-1">
                             <a href="https:www.google.com" title="Facebook" className="flex items-center p-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-5 h-5 fillCurrent">
@@ -104,7 +100,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="py-6 text-sm text-center dark:text-coolGray-400">©️ 1968 Company Co. All rights reserved.</div>
+            <div className="py-6 text-md font-bold text-center ">©️ 2020 Company Co. All Rights Reserved.</div>
         </div>
     );
 };
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
+import './Header.css'
 
 const Header = () => {
     const { user, logOut } = useAuth();
     return (
         <div>
-            <nav className=" bg-blue-700 flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <nav className="header-bg flex items-center justify-between flex-wrap bg-teal-500 p-7">
                 <div className="flex items-center flex-shrink-0 text-white mr-6">
-                    <span className="font-serif lg:flex-grow text-2xl">Travelata</span>
+                    <span className="font-serif lg:flex-grow text-3xl">TRAVELATA</span>
                 </div>
                 <div className="block lg:hidden">
                     <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
@@ -16,32 +17,32 @@ const Header = () => {
                     </button>
                 </div>
                 <div className="w-full block flex-grow-0 lg:flex lg:items-center lg:w-auto">
-                    <div className="font-serif lg:flex-grow text-2xl ">
-                        <Link to='/home' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow">
+                    <div className="font-serif lg:flex-grow text-xl ">
+                        <Link to='/home' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:bg-yellow-400 p-2 rounded-lg mr-4">
                             Home
                         </Link>
-                        {user.email && <Link to='/mybookings' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow">
+                        {user.email && <Link to='/mybookings' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:bg-yellow-400 p-2 rounded-lg mr-4">
                             My Bookings
                         </Link>}
-                        {user.email && <Link to='/manageallbookings' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow">
-                            Manage All Bookings
+                        {user.email && <Link to='/manageallbookings' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:bg-yellow-400 p-2 rounded-lg mr-4">
+                            Manage All
                         </Link>}
-                        {user.email && <Link to='/addnewbooking' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 hover:animate-bounce-slow">
-                            Add A New Booking
+                        {user.email && <Link to='/addnewbooking' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:bg-yellow-400 p-2 rounded-lg mr-4">
+                            Add Package
                         </Link>}
-                        <Link to='/about' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white hover:animate-bounce-slow mr-2">
-                            About Us
+                        <Link to='/about' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:bg-yellow-400 p-2 rounded-lg mr-4">
+                            About
                         </Link>
-                        {user.email && <p className='block border-2 hover:animate-bounce-slow hover:bg-blue-300 hover:text-black lg:inline-block lg:mt-0 mr-2 mt-4 p-1 rounded text-teal-200 text-yellow-500 sm:w-52 lg:text-center sm:text-center'>{user.displayName}</p>}
+                        {user.email && <p className='block text-yellow-400  hover:text-white lg:inline-block lg:mt-0 mr-2 mt-4 text-xl font-bold text-teal-200'>{user.displayName}</p>}
 
 
                         {user.email ? <Link to='/login' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-2">
-                            <button onClick={logOut} className='border-2 focus:ring-2 focus:ring-gray-400 hover:bg-blue-400 hover:text-black p-1 rounded'> Log Out</button> </Link> :
+                            <button onClick={logOut} className='border-2 focus:ring-2 focus:ring-gray-400 hover:bg-yellow-400 hover:text-black px-1 rounded'> Log Out</button> </Link> :
 
 
 
                             <Link to='/login' className=" text-white block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-2">
-                                <button className='border-2 focus:ring-2 focus:ring-gray-400 hover:bg-blue-400 hover:text-black p-1 rounded'>Login</button> </Link>}
+                                <button className='border-2 focus:ring-2 focus:ring-gray-400 hover:bg-yellow-400 hover:text-black px-1 rounded'>Login</button> </Link>}
 
 
                     </div>
